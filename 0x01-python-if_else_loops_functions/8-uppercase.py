069717a7d56d5634d7a83370836726fb0f22bb13
AndresSern <andrecamposer@gmail.com>
2020-12-08 19:50:48 +0000

    If is lower convert in upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        i = ord(i)
+        if i >= 97 and i <= 122:
+            i -= 32
+        i = chr(i)
+        print("{}".format(i), end='')
+    print("")
