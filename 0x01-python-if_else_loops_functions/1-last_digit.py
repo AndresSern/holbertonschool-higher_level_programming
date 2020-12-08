bfbe24b7f2f208df7826fc464ed9c5c1f921ba7f
AndresSern <andrecamposer@gmail.com>
2020-12-08 17:53:13 +0000

    Testing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ if lastN > 5:
 elif lastN == 0:
     print("{}{:d} is {:d} and is 0".format(string, number, lastN))
 else:
-    print("{}{:d} is {:d} and is less than 6 and not 0".format(string,
+    print("{}{:d} is {:d} and is less than 6 and not ".format(string,
                                                                number,
                                                                lastN))
