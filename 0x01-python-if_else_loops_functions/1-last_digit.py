e3b4b628e671e15e2b4e6cbb077568e9e60630e3
AndresSern <andrecamposer@gmail.com>
2020-12-08 18:03:00 +0000

    Putting an aditional condtional 'number < 0'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 lastN = number % 10
 string = "Last digit of "
+if number < 0:
+    lastN = lastN * -1
 if lastN > 5:
     print("{}{:d} is {:d} and is greater than 5".format(string, number, lastN))
 elif lastN == 0:
     print("{}{:d} is {:d} and is 0".format(string, number, lastN))
 else:
-    lastN = lastN * -1
     print("{}{:d} is {:d} and is less than 6 and not 0".format(string,
                                                               number,
                                                               lastN))
