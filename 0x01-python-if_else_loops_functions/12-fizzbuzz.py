1cb6cc82dabdd98c51f5e493c724c312c7b44969
AndresSern <andrecamposer@gmail.com>
2020-12-08 20:29:11 +0000

    Putting space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     while(count < 100):
 
         if count % 5 == 0 and count % 3 == 0:
-            print("FizzBuzz ",end='')
+            print("FizzBuzz ", end='')
         elif count % 3 == 0:
             print("Fizz ", end='')
         elif count % 5 == 0:
