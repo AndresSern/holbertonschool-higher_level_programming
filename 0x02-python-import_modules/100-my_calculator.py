76877383c50ee3b47024a2234693183ca82e7876
AndresSern <andrecamposer@gmail.com>
2020-12-11 05:34:02 +0000

    Putting variable down of the first conditional

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,12 +3,13 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    numb1 = int(argv[1])
-    numb2 = int(argv[3])
-
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    numb1 = int(argv[1])
+    numb2 = int(argv[3])
+
     if argv[2] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
