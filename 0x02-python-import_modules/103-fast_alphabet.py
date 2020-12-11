ed6af470e2aea1cfea0ab318569208d7a56aaa2c
AndresSern <andrecamposer@gmail.com>
2020-12-11 04:20:01 +0000

    Creating and removing files

--- 0x02-python-import_modules/103-fast_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-from fast_alphabet import printalphabet
-printalphabet()
--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-sum = 0
-for a in range(1, len(sys.argv)):
-    sum += int(sys.argv[a])
-print(sum)
