f8dc1f263c9a882909d5e4f22635fd522db9a906
AndresSern <andrecamposer@gmail.com>
2020-12-15 23:30:43 +0000

    Test

--- 0x03-python-data_structures/new.py
+++ 0x03-python-data_structures/new.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for count, j in enumerate(i):
+        count = 0
+        for j in i:
             print("{:d}".format(j), end="")
             if count != (len(i) - 1):
                 print("{}".format(" "), end="")
