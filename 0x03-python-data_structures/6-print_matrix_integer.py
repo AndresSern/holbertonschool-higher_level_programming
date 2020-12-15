af6ed67ef104f31398d0470dd54f9403b4096461
AndresSern <andrecamposer@gmail.com>
2020-12-15 18:30:11 +0000

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         count = 0
         for j in i:
-            print("{}".format(j), end="")
+            print("{:d}".format(j), end="")
             if count != (len(matrix) - 1):
                 print(" ", end="")
             count += 1
--- 0x03-python-data_structures/test/10-divisible_by_2.py
+++ 0x03-python-data_structures/test/10-divisible_by_2.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
+    count = 0
     for i in my_list:
         if i % 2 == 0:
-            new_list.append(True)
+            new_list[count] = True
         else:
-            new_list.append(False)
+            new_list[count] = False
+        count += 1
     return new_list
+
+
