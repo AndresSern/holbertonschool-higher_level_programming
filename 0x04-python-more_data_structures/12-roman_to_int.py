c69c8cf2a5c627bf9398c1fe1478cdd91c85b3ce
AndresSern <andrecamposer@gmail.com>
2020-12-18 22:19:39 +0000

    Putting conditional if there are 4 letters equals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,21 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    actual = 0
-    anterior = 0
+    number_current = 0
+    number_before = 0
+    character_before = ""
+    count = 0
     for i in roman_string:
         if i in my_dict:
-            actual += my_dict[i]
-            if anterior < my_dict[i]:
-                actual -= anterior * 2
-            anterior = my_dict[i]
-    return actual
+            number_current += my_dict[i]
+            if number_before < my_dict[i]:
+                number_current -= number_before * 2
+            number_before = my_dict[i]
+            if i == character_before:
+                count += 1
+                if count == 3:
+                    return 0
+            else:
+                count = 0
+            character_before = i[:]
+    return number_current
