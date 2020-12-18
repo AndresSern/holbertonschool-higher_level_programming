d03dc6a914e8aeefb170fe2a21e34a89b500c618
AndresSern <andrecamposer@gmail.com>
2020-12-18 08:56:55 +0000

    Removing variable

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiplicate = list(map(lambda x: x * number, my_list))
-    return multiplicate
+    return list(map(lambda x: x * number, my_list))
