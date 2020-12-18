3aa3d93982fa26c88f056c37db87f7ad479ef9cb
AndresSern <andrecamposer@gmail.com>
2020-12-18 21:57:03 +0000

    Numbers romans

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    actual = 0
+    anterior = 0
+    for i in roman_string:
+        if i in my_dict:
+            if anterior < my_dict[i]:
+                actual -= anterior
+            actual += my_dict[i]
+            anterior = my_dict[i]
+        return actual
