c5a51cfd3f9ba3d85d6e9352d46d84be84286707
AndresSern <andrecamposer@gmail.com>
2021-01-05 00:46:43 +0000

    Erasig, moving files and creating the file 102-magic_calcultation.py

--- 0x05-python-exceptions/prueba.py
+++ 0x05-python-exceptions/prueba.py
@@ -9,13 +9,9 @@
  *
  """
 a = 10
-b =
-try:
-    # condition for checking for negative values
-    if a < 0 or b < 0:
-        # raising exception using raise keyword
-        raise ZeroDivisionError
-    print(a/b)
-except ZeroDivisionError:
-    print("Please enter valid integer value")
+b = "2"
 
+try:
+    print("{:d}".format(b))
+except ZeroDivisionError:
+    print("hola")
