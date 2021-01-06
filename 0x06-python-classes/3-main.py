aa795890316209e82696381f6e5c6ff6a4ad948f
AndresSern <andrecamposer@gmail.com>
2021-01-06 01:21:33 +0000

    Area of A Square

---
+++ 0x06-python-classes/3-main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+Square = __import__('3-square').Square
+
+my_square_1 = Square(2)
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+    print(my_sqaure)
+except Exception as e:
+    print(e)
+
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a class Square that defines a square by: (based on 1-square.py)
+Write a class Square that defines a square by: (based on 2-square.py)
 """
 
 
@@ -12,3 +12,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    """Defining the square"""
+    def area(self):
+        return self.__size * self.__size
