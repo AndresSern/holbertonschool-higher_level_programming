a9ae0aa02ff2f2ae3e47c3f56d19d701c814eb5b
AndresSern <andrecamposer@gmail.com>
2021-01-11 20:40:05 +0000

    Creating files and making the function 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+        This class doesn´t nothing
+    """
+    pass
+
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+This is readme is about more object
+
