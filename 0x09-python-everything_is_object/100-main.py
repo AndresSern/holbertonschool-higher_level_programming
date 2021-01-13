9784f0ed5f023dd43df3b481d79461b5af6cc179
AndresSern <andrecamposer@gmail.com>
2021-01-13 00:26:35 +0000

    Addind a space to the side ,

--- 0x09-python-everything_is_object/100-main.py
+++ 0x09-python-everything_is_object/100-main.py
@@ -2,8 +2,5 @@
 magic_string = __import__('100-magic_string').magic_string
 
 for i in range(10):
-    print("--(({})----{})---".format(magic_string(), i))
-    print(id(magic_string()))
+    print(magic_string())
 
-
-print(id(magic_string()))
