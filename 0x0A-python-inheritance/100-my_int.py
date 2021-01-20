0468b4a55f786917b8f3ff642fb52a2f1be61832
AndresSern <andrecamposer@gmail.com>
2021-01-20 02:37:57 +0000

    Update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Write a class MyInt that inherits from int:
+
+    *MyInt is a rebel. MyInt has == and != operators inverted
+    *You are not allowed to import any module
+"""
+
+
 class MyInt(int):
+    """This class invert the operators"""
     def __ne__(self, other):
         return not self.__eq__(other)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,19 +2,7 @@
 """
 Class Called BaseGeometry
 """
-
-
-class BaseGeometry:
-    """This is a class called BaseGeometry"""
-    def area(self):
-        """This Function Raise an Error"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be a greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
