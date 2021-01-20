74da53ed40fc89a800e8b239dbf47fb9adb9d925
AndresSern <andrecamposer@gmail.com>
2021-01-20 01:24:14 +0000

    Implement the function area

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,6 +15,10 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
+    def area(self):
+        '''Calculates the area of a square'''
+        return self.__size * self.__size
+
     def __str__(self):
         """This Function Return A Description about the square"""
         return ("[Square] {}/{}".format(self.__size, self.__size))
