1040e222ebb958ce0e01a005efe54e879afa28bc
AndresSern <andrecamposer@gmail.com>
2021-01-20 01:08:15 +0000

    Import Rectangle in the file number 9

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,40 +2,7 @@
 """
 Class Called BaseGeometry
 """
-
-
-class BaseGeometry:
-    """This is a class called BaseGeometry"""
-
-    def area(self):
-        """This Function Raise an Error"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be a greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """This Class called Rectangle inherits from BaseGeometry"""
-
-    def __init__(self, width, height):
-        """This is the Instantiation"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """This function is the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ This function return to a descripcion about this class"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """This class is Square that inherits from Rectangle"""
