2d0b1d07ebee1636c322e7303a77a686d6015afa
AndresSern <andrecamposer@gmail.com>
2021-01-20 03:48:39 +0000

    Pep8

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,12 +3,17 @@
 
     *MyInt is a rebel. MyInt has == and != operators inverted
     *You are not allowed to import any module
+
+
 """
 
 
 class MyInt(int):
     """This class invert the operators"""
     def __eq__(self, other):
+        """Overwrite to false """
         return False
+
     def __ne__(self, other):
+        """OverWrite to true """
         return True
