48e96ce9077d256e48544d20d86b02acf5a1ff65
AndresSern <andrecamposer@gmail.com>
2021-01-20 03:33:23 +0000

    Adding files 1-test.txt

--- 0x0A-python-inheritance/mains/7-main.py
+++ 0x0A-python-inheritance/mains/7-main.py
@@ -3,7 +3,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", 12)
+print(bg.integer_validator("my_int", 12))
 bg.integer_validator("width", 89)
 
 try:
