823c18fec31aa23b4b0054eb3b5cf67f9b537a7d
AndresSern <andrecamposer@gmail.com>
2021-01-21 02:07:31 +0000

    If The FIle Exist

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,17 @@
         *If the file doesnt exist, it should be created
 """
 from sys import argv
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 i = len(argv)
+filename = "add_item.json"
 if i == 1:
-    save_to_json_file([], "add_item.json")
+    if not path.exists(filename):
+        save_to_json_file([], filename)
 else:
-    list_arguments = load_from_json_file("add_item.json")
+    list_arguments = load_from_json_file(filename)
     for counter in range(1, i):
         list_arguments.append(argv[counter])
-    save_to_json_file(list_arguments, "add_item.json")
+    save_to_json_file(list_arguments, filename)
