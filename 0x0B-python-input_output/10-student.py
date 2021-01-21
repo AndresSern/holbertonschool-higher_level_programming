61bf5b7d43abaac93c35f207784d199254ef4f4f
AndresSern <andrecamposer@gmail.com>
2021-01-21 04:10:09 +0000

    Return to my_dict

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Write a class Student that defines a student by"""
 
-
 class Student:
     def __init__(self, first_name, last_name, age):
         """This function init the class"""
@@ -24,6 +23,4 @@ class Student:
         for i in attrs:
             if i in self.__dict__:
                my_dict[i] = self.__dict__.get(i)
-        if len(my_dict) > 0:
-            return my_dict
-        return self.__dict__
+        return my_dict
