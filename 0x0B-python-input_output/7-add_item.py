9122f8b4723d2387759f0b9ccf631be19fe78935
AndresSern <andrecamposer@gmail.com>
2021-01-21 04:44:54 +0000

    Testing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,8 @@
         *If the file doesnt exist, it should be created
 """
 from sys import argv
-from os import path
-save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 i = len(argv)
 filename = "add_item.json"
