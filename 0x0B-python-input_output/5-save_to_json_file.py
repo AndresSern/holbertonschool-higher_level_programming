231eed9e8b17eee43d5ee59112f9ab0711c72d2b
AndresSern <andrecamposer@gmail.com>
2021-01-21 01:59:43 +0000

    Removing indent

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """ Write a function that returns an object (Python data structure)
         represented by a JSON string:"""
     with open(filename, 'w') as filename:
-        json.dump(my_obj, filename, indent=4)
+        json.dump(my_obj, filename)
