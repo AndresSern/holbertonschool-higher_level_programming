0eebc97088a1d4ea00323cec4a73a1fd3643e5ce
AndresSern <andrecamposer@gmail.com>
2021-01-21 23:18:41 +0000

    Pep8

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a function that inserts a line of text to a file, after each line containing a specific string """
+"""Write a function that inserts a line of text to a file, after each
+   line containing a specific string """
 
 
 def append_after(filename="", search_string="", new_string=""):
@@ -13,4 +14,3 @@ def append_after(filename="", search_string="", new_string=""):
                 new_file_lines.append(new_string)
     with open(filename, 'w') as f:
         f.writelines(new_file_lines)
-
