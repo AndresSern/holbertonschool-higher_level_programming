7191939073cddbc6a2e3136c300244f65347e0bf
AndresSern <andrecamposer@gmail.com>
2021-01-21 03:19:03 +0000

    Changed dict to dir

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,5 +6,4 @@ def class_to_json(obj):
     '''Function that
     returns the dictionary description with simple data
     structure for JSON serialization of an object'''
-
-    return obj.__dir__
+    return obj.__dict__
