8003057a282ac5aa2cbabd18767e62f8961cd9ea
AndresSern <andrecamposer@gmail.com>
2021-01-21 02:20:50 +0000

    Update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,13 @@ from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-i = len(argv)
 filename = "add_item.json"
-if i == 1:
-    if not path.exists(filename):
-        save_to_json_file([], filename)
-else:
-    list_arguments = load_from_json_file(filename)
-    for counter in range(1, i):
-        list_arguments.append(argv[counter])
-    save_to_json_file(list_arguments, filename)
+try:
+    my_items = load_from_json_file(filename)
+
+except FileNotFoundError:
+    my_items = []
+for i in range(1, len(argv)):
+    my_items.append(argv[i])
+
+save_to_json_file(my_items, filename)
