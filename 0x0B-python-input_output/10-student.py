142f402c8d3fcab18218dc7a10440dfa6ef1fb31
AndresSern <andrecamposer@gmail.com>
2021-01-21 03:57:43 +0000

    Adding the file 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """This function init the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained in
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved"""
+        if not attrs:
+            return self.__dict__
+        for i in attrs:
+            if type(i) is not str:
+                return self.__dict__
+        my_dict = {}
+        for i in attrs:
+            if i in self.__dict__:
+               my_dict[i] = self.__dict__.get(i)
+        if len(my_dict) > 0:
+            return my_dict
+        return self.__dict__
