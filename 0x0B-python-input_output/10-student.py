f5ff908133517f5ae9249746126ccf7f87028b3d
AndresSern <andrecamposer@gmail.com>
2021-01-21 04:17:15 +0000

    Update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write a class Student that defines a student by"""
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
         """This function init the class"""
@@ -12,12 +13,11 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance
         If attrs is a list of strings, only attribute names contained in
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved"""
-        if not attrs:
+        this list must be retriev"""
+        if attrs is None:
             return self.__dict__
         my_dict = {}
         for i in attrs:
             if i in self.__dict__:
-               my_dict[i] = self.__dict__.get(i)
+                my_dict[i] = self.__dict__.get(i)
         return my_dict
