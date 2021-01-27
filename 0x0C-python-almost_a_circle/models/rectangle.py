7c24b0bf0ffa61ed713dc2ccc007c3f6ac1ffbb7
AndresSern <andrecamposer@gmail.com>
2021-01-27 14:58:11 +0000

    Comments in rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+""" Write the class Rectangle that inherits from Base:"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ Class About Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
