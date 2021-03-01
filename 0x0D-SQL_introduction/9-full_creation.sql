513cdeacd265a56c19ebcd258c9a32994a475296
AndresSern <andrecamposer@gmail.com>
2021-03-01 20:37:04 +0000

    FIx the comment and the file number 9 and adding the file 10-top_score.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
---Second_table description, id INT, name VARCHAR(256), score INT
+-- Second_table description, id INT, name VARCHAR(256), score INT
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(56), score int)
-INSERT INTO second_table(id, name, score) VALUE(1, "John", 10), (2, "Alex", 3),(3, "Bob", 14), (4, "George", 8)
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(56), score int);
+INSERT INTO second_table(id, name, score) VALUE(1, "John", 10), (2, "Alex", 3),(3, "Bob", 14), (4, "George", 8);
