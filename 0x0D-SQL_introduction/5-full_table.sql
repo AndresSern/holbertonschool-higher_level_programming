1901de7f05074a7a69be0551f932573e7778bc51
AndresSern <andrecamposer@gmail.com>
2021-03-01 17:59:26 +0000

    Adding the file 5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script that creates a table called first_table in the current database in your MySQL server.
+-- If the table first_table already exists, your script should not fail
+SHOW CREATE TABLE first_table;
