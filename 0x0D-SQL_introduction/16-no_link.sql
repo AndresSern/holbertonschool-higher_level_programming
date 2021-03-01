0729d8bf0d5979ce2b11cd2b38ce5cd1daf7f0e4
AndresSern <andrecamposer@gmail.com>
2021-03-01 22:26:30 +0000

    Putting order desc by score

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -4,4 +4,4 @@
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
 -- In this example, new data have been added to the table second_table.
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
