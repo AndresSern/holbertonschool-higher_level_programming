3d7fec3ac500979bb5badd99d3f4c1e65fe32f35
AndresSern <andrecamposer@gmail.com>
2021-03-01 16:53:03 +0000

    Adding the file 3-lists_tables.sql

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql:  "cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql"
+SHOW TABLES  
