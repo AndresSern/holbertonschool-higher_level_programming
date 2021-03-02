3916d7dea00d8dba6b46d1d3fc752a05618d3340
AndresSern <andrecamposer@gmail.com>
2021-03-02 17:46:26 +0000

    Adding the file 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Write a script that creates the table unique_id on your MySQL server.
+--  unique_id description:
+--  id INT with the default value 1 and must be unique
+--  name VARCHAR(256)
+--  The database name will be passed as an argument of the mysql command
+--  If the table unique_id already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	)
