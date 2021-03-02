a0050ba260e5fbf48d7f162bc062fccfa51cd58e
AndresSern <andrecamposer@gmail.com>
2021-03-02 17:59:52 +0000

    Adding the file 7-cities.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,7 @@
 --  If the table unique_id already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+	id INT AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
 	)
