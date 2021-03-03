a91feaa57af4a871e19d59c6b5167f90a86f5bae
AndresSern <andrecamposer@gmail.com>
2021-03-03 04:29:20 +0000

    Changing from By FRom

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,7 +6,7 @@
 --	Results must be sorted in descending order by the number of shows linked
 --	You can use only one SELECT statement
 --	The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name from tv_show_genres
+SELECT tv_genres.name FROM tv_show_genres
        JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
        JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
        WHERE tv_shows.title = "DExter"
