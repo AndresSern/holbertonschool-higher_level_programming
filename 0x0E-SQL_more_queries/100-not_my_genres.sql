a328ca79725ff4d82bd497a293413adeabc28179
AndresSern <andrecamposer@gmail.com>
2021-03-03 20:31:13 +0000

    Removing the name of the tables

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@
 --	Each record should display: tv_shows.title - tv_genres.name
 --	Results must be sorted in ascending order by the show title and genre name
 --	You can use only one SELECT statement
-SELECT tv_genres.name from tv_genres
+SELECT tv_genres.name FROM tv_genres
        WHERE tv_genres.id  NOT IN (SELECT genre_id FROM tv_show_genres
        RIGHT JOIN tv_shows ON show_id = id 
        WHERE title = "Dexter") ORDER BY tv_genres.name ASC;
