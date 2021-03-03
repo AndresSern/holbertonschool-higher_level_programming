3f730d7300c25ce636b791acb50e720ce10cd313
AndresSern <andrecamposer@gmail.com>
2021-03-03 23:53:18 +0000

    PUtting a space in the comment

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
---Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 --	Each record should display: tv_genres.name - rating sum
 -- 	Results must be sorted in descending order by their rating
 --	You can use only one SELECT statement
