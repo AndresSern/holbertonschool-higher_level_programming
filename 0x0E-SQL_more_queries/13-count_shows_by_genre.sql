03a93d4eada9a55abd3ac90bae8fc7554412d719
AndresSern <andrecamposer@gmail.com>
2021-03-03 02:21:58 +0000

    Changing inner by left

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,6 +6,6 @@
 --	Results must be sorted in descending order by the number of shows linked
 --	You can use only one SELECT statement
 --	The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, count(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres 
-       INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-       GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, count(tv_show_genres.show_id) AS number_of_shows FROM tv_genres 
+       LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
