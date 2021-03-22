84d8e85fa845c0096e2bf238fca402584dd039b4
AndresSern <andrecamposern@gmail.com>
2021-03-22 15:29:48 +0000

    Adding the executable 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* Script that prints two arguments passed to it, in the following format:"is" */
+const item1 = process.argv[2];
+const item2 = process.argv[3];
+console.log(`${item1} is ${item2}`);
