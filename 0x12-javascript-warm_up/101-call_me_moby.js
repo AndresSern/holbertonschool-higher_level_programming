e9abb95009ca31bd2ec47bd06cc1f82d982c4f6c
AndresSern <andrecamposern@gmail.com>
2021-03-22 20:31:23 +0000

    Adding a space in the comment

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* Function that executes 'number of times' a function*/
+/* Function that executes 'number of times' a function */
 exports.callMeMoby = function (repeat, theFunction) {
   for (let i = 0; i < repeat; i++) {
     theFunction();
