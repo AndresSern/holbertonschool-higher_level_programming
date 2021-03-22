8df2a3f02ee2549ff0545fc0e4f85b65666679ae
AndresSern <andrecamposern@gmail.com>
2021-03-22 20:30:32 +0000

    Adding the function 101-call_me_moby.js

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Function that executes 'number of times' a function*/
+exports.callMeMoby = function (repeat, theFunction) {
+  for (let i = 0; i < repeat; i++) {
+    theFunction();
+  }
+};
