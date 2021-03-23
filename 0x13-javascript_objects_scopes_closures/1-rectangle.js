b8275a6c4f55b473f5c28b3f112ca145a632dd1c
AndresSern <andrecamposer@gmail.com>
2021-03-23 10:27:30 -0500

    Fixing style with standard

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* Write a class Rectangle that defines a rectangle */
 class Rectangle {
-  constructor(w, h){
-  this.width = w;
-  this.height = h;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
