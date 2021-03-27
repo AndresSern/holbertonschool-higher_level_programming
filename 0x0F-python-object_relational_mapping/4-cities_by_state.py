2c79311ca49dc9b82cff0cc7d973ea011ec8fb9e
AndresSern <andrecamposern@gmail.com>
2021-03-27 23:51:10 +0000

    Adding conditional if and fixing pep8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,19 +5,22 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost",  # your host
-                     user=argv[1],       # username
-                     passwd=argv[2],     # password
-                     db=argv[3],
-                     port=3306)   # name of the database
-cur = db.cursor()
+if __name__ == "__main__":
+    """ Code that takes an argument and dipslays all values in the sates
+        table of 'hbtn_0_e_0_usa' where name matches the argument """
+    db = MySQLdb.connect(host="localhost",  # your host
+                         user=argv[1],       # username
+                         passwd=argv[2],     # password
+                         db=argv[3],
+                         port=3306)   # name of the database
+    cur = db.cursor()
 
-cur.execute("""SELECT cities.id, cities.name, states.name
-               FROM cities INNER JOIN states
-               ON cities.state_id = states.id
-               ORDER BY cities.id ASC;""");
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                   FROM cities INNER JOIN states
+                   ON cities.state_id = states.id
+                   ORDER BY cities.id ASC;""")
 
-for row in cur.fetchall():
-    print(row)
+    for row in cur.fetchall():
+        print(row)
 
-cur.close()
+    cur.close()
