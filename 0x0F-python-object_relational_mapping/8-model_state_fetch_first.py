e94495061943eb3414fc6a2c23ec0f6ec222cd48
AndresSern <andrecamposern@gmail.com>
2021-03-28 00:50:26 +0000

    Fixing pep8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,10 +9,15 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """ Start link class to table in database """
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{} """
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).first()
+    try:
+        state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(state.id, state.name))
+    except Exception:
+        print("Nothing")
+    session.close()
