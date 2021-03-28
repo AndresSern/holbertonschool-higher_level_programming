6185964e6c2c885dcdc754e2147d48f1d660e317
AndresSern <andrecamposern@gmail.com>
2021-03-28 01:02:11 +0000

    Chaging state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        state = session.query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
+    if state:
         print("{}: {}".format(state.id, state.name))
-    except Exception:
+    else:
         print("Nothing")
     session.close()
