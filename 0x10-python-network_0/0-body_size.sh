1d6c18d10afbd488402bd97b8fe378507dc96e8f
AndresSern <andrecamposer@gmail.com>
2021-04-08 12:16:36 -0500

    sheckscript in the file 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-
+# Takes in a URL sends a request to that URL and displays the size of the body
 curl -so /dev/null $1 -w '%{size_download}\n'
 
