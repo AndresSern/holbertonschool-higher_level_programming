e6546d9a3f898b6b033f4700cbdf7c56f09204b4
AndresSern <andrecamposer@gmail.com>
2021-04-08 12:20:16 -0500

    Comment changed and removing the line 4

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-
+# takes in a URL sends a GET request to URL and displays the body the response
 curl -so /dev/null $1 -w '%{size_download}\n'
-
