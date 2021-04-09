0199a0632d23a38a33822f526722d512ab0e1865
AndresSern <andrecamposer@gmail.com>
2021-04-08 23:37:35 -0500

    Removing double points and putting equals

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a url, sends a POST request to the passed URL and display the body of the response
-curl -sX POST -d "email: hr@holbertonschool.com" -d "subject: I will always be here for PLD" $1
+curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" $1
