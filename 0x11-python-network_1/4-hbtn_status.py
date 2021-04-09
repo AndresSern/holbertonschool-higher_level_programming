4b2420c9d577679a1c0d5f299ef006d9b1671794
AndresSern <andrecamposer@gmail.com>
2021-04-09 13:57:46 -0500

    Removing the library sys

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,6 @@
 Script that fetches https://intranet.hbtn.io/status
 """
 import requests
-from sys import argv
 
 if __name__ == "__main__":
     res = requests.get("https://intranet.hbtn.io/status")
