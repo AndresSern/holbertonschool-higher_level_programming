e9983decb9e0a051153efea5fa32416a0f69b6f9
AndresSern <andrecamposer@gmail.com>
2021-04-09 13:30:52 -0500

    Fixing pep 8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ from urllib import request, parse
 from sys import argv
 
 if __name__ == "__main__":
-    values = {'email' : argv[2]}
-    data =  parse.urlencode(values)
+    values = {'email': argv[2]}
+    data = parse.urlencode(values)
     data = data.encode("ascii")
     req = request.Request(argv[1], data)
     with request.urlopen(req) as respons:
