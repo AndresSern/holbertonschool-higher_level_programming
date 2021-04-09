72a675ffbbadf4c2521952bbfeb6e7f9d607db9d
AndresSern <andrecamposer@gmail.com>
2021-04-09 12:10:27 -0500

    REmoving indententation in the prints

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
+from urllib import request
 
 if __name__ == "__main__":
-    from urllib import request
 
     req = request.Request("https://intranet.hbtn.io/status")
 
     with request.urlopen(req) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
