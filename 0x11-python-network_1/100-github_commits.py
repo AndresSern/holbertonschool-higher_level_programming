63284140e768ea3743ad26a4fd575edfd6bbfc3a
AndresSern <andrecamposer@gmail.com>
2021-04-09 20:39:40 -0500

    Chaging the number 9 by a 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,14 +8,13 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
-
     repository = argv[1]
     name = argv[2]
     repository_name = "{}/{}/commits".format(name, repository)
     res = requests.get("https://api.github.com/repos/" + repository_name)
     commit_json = res.json()
     for i, commit_current in enumerate(commit_json):
-        if i == 9:
+        if i == 10:
             break
         idCommit = commit_current["sha"]
         nameCommit = commit_current["commit"]["author"]["name"]
