a490cce8701bf373b11a15bc5650dbc9001839a9
AndresSern <andrecamposer@gmail.com>
2021-04-12 13:56:45 -0500

    Putting .all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,14 +8,18 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
     engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
-                           .format(argv[1], argv[2], argv[3]),
+                           .format(username, password, database_name),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id.asc())
+    result = session.query(State).order_by(State.id.asc().all())
 
     for row in result:
         print("{} : {}".format(row.id, row.name))
+    session.close()
