1fb8cfca0547d1ce96eb5a78478e613dc2fa1a7a
AndresSern <andrecamposer@gmail.com>
2021-05-04 13:29:18 -0500

    Adding the file 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+ * Script that prints the number of movies where the character "Wedge Antilles"
+ * is present
+*/
+
+const url = 'https://swapi-api.hbtn.io/api/films';
+const request = require('request');
+
+request(url, function (err, result, body) {
+  const films = body;
+  const ocurrencesFilms = films.split('/people/18/').length - 1;
+  console.log(ocurrencesFilms);
+  if (err) {
+    console.log(err);
+  }
+});
