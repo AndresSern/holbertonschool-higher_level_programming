3c9e8683e2e3db5b4dc78c86cce87977a3bf9d56
AndresSern <andrecamposer@gmail.com>
2021-05-04 15:19:41 -0500

    ADding a console.log to the conditional if(err)

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,13 +9,14 @@ const request = require('request');
 request(url, function (err, result, body) {
   const films = JSON.parse(body).results;
   let count = 0;
-  for (let i = 0; i < films.length; i++){
-    tempCount = films[i].characters.toString().match(/people\/18/g);
-    if(tempCount !== null){
-       count = count + tempCount.length;
+  for (let i = 0; i < films.length; i++) {
+    const tempCount = films[i].characters.toString().match(/people\/18/g);
+    if (tempCount !== null) {
+      count = count + tempCount.length;
     }
   }
   console.log(count);
   if (err) {
+    console.log(err);
   }
 });
