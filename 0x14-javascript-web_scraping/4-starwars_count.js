7a68874e92169048dc6f6bcd58dd8b751519d73c
AndresSern <andrecamposer@gmail.com>
2021-05-04 15:17:45 -0500

    Adding a slash

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request(url, function (err, result, body) {
   const films = JSON.parse(body).results;
   let count = 0;
   for (let i = 0; i < films.length; i++){
-    tempCount = films[i].characters.toString().match(/18/g);
+    tempCount = films[i].characters.toString().match(/people\/18/g);
     if(tempCount !== null){
        count = count + tempCount.length;
     }
