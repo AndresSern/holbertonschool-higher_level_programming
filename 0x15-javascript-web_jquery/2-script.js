62080ea2c02feb20d4481f00bd37ccbe5a212bb0
Andrés Felipe <andrecamposer@gmail.com>
2021-05-12 11:15:02 -0500

    Adding the file numer 4html and 4js with its functions

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,8 @@
 /* Script that updates the text color of the <header> element to red 
 (#FF0000) when the user clicks on the tag DIV#red_header */
-divRedHeader = $("div#red_header");
-divRedHeader.click(function () {
-	divRedHeader.css("color", "#FF0000");
+$(document).ready(function () {
+	divRedHeader = $("div#red_header");
+	divRedHeader.click(function () {
+		divRedHeader.css("color", "#FF0000");
+	});
 });
