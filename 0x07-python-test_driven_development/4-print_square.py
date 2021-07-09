9ddf040f073e4924094502e64d1925f6e2f92bcc
AndresSern <andrecamposer@gmail.com>
2021-07-09 16:29:04 -0500

    CHaging i by '_'

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,5 +10,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size):
+    for _ in range(size):
         print("#" * size)
