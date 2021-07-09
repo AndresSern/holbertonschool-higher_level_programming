4e1e9c93dc68a404cce098ad0f397c5fd1d8538c
AndresSern <andrecamposer@gmail.com>
2021-07-09 16:38:22 -0500

    Changing type by insnstance

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,25 +9,29 @@ def matrix_divided(matrix, div):
     Function That Divides All Elementes of a Matrix
     """
     error = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) != list or len(matrix) < 1:
+
+    if isinstance(matrix, list) or len(matrix) < 1:
         raise TypeError(error)
     if div is 0:
         raise ZeroDivisionError("division by zero")
-    if type(div) not in [int, float]:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if type(matrix[0]) is not list:
+    if not isinstance(matrix[0], list):
             raise TypeError(error)
+
     len_matrix = len(matrix[0])
     new_list = []
+
     for count, i in enumerate(matrix):
-        if type(i) is not list or len(i) < 1:
+        if isinstance(i, list) or len(i) < 1:
             raise TypeError(error)
 
         if len(i) != len_matrix:
             raise TypeError("Each row of the matrix must have the same size")
+
         new_list.append([])
         for j in i:
-            if type(j) not in [int, float]:
+            if isinstance(j, (int, float)):
                 raise TypeError(error)
             new_list[count].append(round(j / div, 2))
     return new_list
